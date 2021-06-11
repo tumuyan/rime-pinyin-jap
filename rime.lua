b4dde4059ca76f4b1618e50bf356c1ad58922cef
tumuyan <tumuyan@gmail.com>
2021-06-11 23:52:41 +0800

    fix
    1. 修复rizi滤镜在高版本opencc中无法工作的错误
    2. 同步rime.lua和zhwiki.dict的上游更新。

--- rime.lua
+++ rime.lua
@@ -1,12 +1,25 @@
 function get_date(input, seg, env)
   if ( input == "date") then
     yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " -"))
-  elseif ( input == "date-") then
+  elseif ( input == "date-"  or  input == "time--") then
     yield(Candidate("date", seg.start, seg._end, os.date("%m/%d"), ""))
     yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
     yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
     yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
-    yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
     yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%B %d"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+  elseif ( input == "time"  or  input == "date---") then
+    yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), " -"))
+    yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " -"))
+    yield(Candidate("time", seg.start, seg._end, os.date("%H%M%S"), " -"))
+  elseif ( input == "time-" or  input == "date--") then
+    yield(Candidate("date", seg.start, seg._end, os.date("%m/%d %H:%M"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d %H:%M"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d %H:%M"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%B %d %H:%M"), ""))
+    yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日 %H:%M"), ""))
   end
 end
